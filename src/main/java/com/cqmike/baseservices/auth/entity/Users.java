3ed15933ad01af63475ede5ca44aae6c9cacbead
cqmike <1535985458@qq.com>
2020-11-09 23:33:19 +0800

    init

---
+++ src/main/java/com/cqmike/baseservices/auth/entity/Users.java
@@ -0,0 +1,13 @@
+package com.cqmike.baseservices.auth.entity;
+
+import com.cqmike.base.entity.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "users")
+public class Users extends BaseEntity {
+
+}
