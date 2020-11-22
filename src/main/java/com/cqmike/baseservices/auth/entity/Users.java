0093ac47db97cbf1bec5faa2501ff850eb6d3619
cqmike <1535985458@qq.com>
2020-11-22 23:23:18 +0800

    feat: add user

--- src/main/java/com/cqmike/baseservices/auth/entity/Users.java
+++ src/main/java/com/cqmike/baseservices/auth/entity/Users.java
@@ -2,12 +2,29 @@ package com.cqmike.baseservices.auth.entity;
 
 import com.cqmike.base.entity.BaseEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
 @Entity
 @Table(name = "users")
 public class Users extends BaseEntity {
+
+    @Column(length = 32, nullable = false, columnDefinition = "DEFAULT '' COMMENT '登录名'")
+    private String userName;
+
+    @Column(length = 32, nullable = false, columnDefinition = "DEFAULT '' COMMENT '用户名'")
+    private String nickName;
+
+    @Column(length = 64, nullable = false, columnDefinition = "DEFAULT '' COMMENT '密码'")
+    private String password;
+
+    @Column(length = 32, columnDefinition = "DEFAULT '' COMMENT '手机号码'")
+    private String phone;
+
+    @Column(length = 32, nullable = false, columnDefinition = "DEFAULT '' COMMENT '邮箱'")
+    private String email;
 
 }
--- src/main/java/com/cqmike/baseservices/auth/security/JwtAccessDeniedHandler.java
+++ src/main/java/com/cqmike/baseservices/auth/security/JwtAccessDeniedHandler.java
@@ -1,5 +1,8 @@
 package com.cqmike.baseservices.auth.security;
 
+import com.cqmike.base.exception.CommonEnum;
+import com.cqmike.base.form.ReturnForm;
+import com.cqmike.base.util.JsonUtils;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -20,7 +23,7 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         //当用户在没有授权的情况下访问受保护的REST资源时，将调用此方法发送403 Forbidden响应
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
+        response.getWriter().write(JsonUtils.toJson(ReturnForm.error(CommonEnum.FORBIDDEN)));
     }
 }
 
